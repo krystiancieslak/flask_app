942cc95f9571dfef6c0aa8d0c6ad9d95c00808b4
Krystian <krystian.cieslak.kontakt@gmail.com>
2022-10-15 19:02:45 +0200

    working on randomized verification

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,6 @@
 from crypt import methods
 from flask import Flask, render_template, request, redirect
+import random
 
 app = Flask(__name__)
 
@@ -19,14 +20,25 @@ def getname():
 
 @app.route("/login", methods=["GET","POST"])
 def login():
+    if request.method == "GET":
+        a=random.randint(0,9)
+        b=random.randint(0,9)
+        c=a+b
+        print(c)
+        print(f"{a}+{b}=")
     if request.method == "POST":
         username = request.form.get('username')
         password = request.form.get('password')
         verify = request.form.get('verify')
-        if (username == "admin") and (password == "admin") and (verify=="4"):
+        print(verify)
+        if (username == "admin") and (password == "admin") and (int(verify)==c):
+            error = None
             return redirect("/")
+        else:
+            error = "Invalid credentials"
+            return error
     else:
-        return render_template("login.html")
+        return render_template("login.html", a=a, b=b)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
