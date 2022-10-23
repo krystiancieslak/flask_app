2cbf354d7f572a1045157ba5a357a5594fc000a8
Krystian <krystian.cieslak.kontakt@gmail.com>
2022-10-23 22:59:16 +0200

    Added permanently logged in function, no need to log in multiple times during one session

--- flask_app.py
+++ flask_app.py
@@ -23,37 +23,25 @@ def getname():
 @app.route("/login", methods=["GET","POST"])
 def login():
     print("-------------------------------NEW REQUEST------------------------------")
-    if request.method == "POST":
-        print("This is POST flow")
-    if request.method == "GET":
-        print("This is GET flow")
-        session["a"] = random.randint(0,9)
-        session["b"] = random.randint(0,9)
+    logged_in=session.get("logged_in", False)
+    if logged_in == True:
+        print("You are logged in!")
+        print("redirecting to / entrypoint...")
+        return redirect("/")
+    else:
+        print("You are not logged in!")
+        if request.method == "POST":
+            print("This is POST flow")
+        if request.method == "GET":
+            print("This is GET flow")
+            session["a"] = random.randint(0,9)
+            session["b"] = random.randint(0,9)
 
-    print("This is happening every time")
+        print("This is happening every time")
 
-    a=session["a"]
-    b=session["b"]
-    username=session.get("username", "Username")
-
-    print("Current values used for checking:")
-    print(f"current value of a: {a}")
-    print(f"current value of b: {b}")
-    print(f"current value of username: {username}")
-    try:
-        print(f"current value of desired_value: {desired_value}")
-    except Exception as e:
-        print (f"Cannot print desired_value, error: {e}")
-
-    if request.method == "POST":
-        print("This is happening only in POST")
-        desired_value = a + b
-        
-        print("Getting username, password and verify value...")
-        username = request.form.get('username')
-        session["username"] = username
-        password = request.form.get('password')
-        verify = request.form.get('verify')
+        a=session["a"]
+        b=session["b"]
+        username=session.get("username", "Username")
 
         print("Current values used for checking:")
         print(f"current value of a: {a}")
@@ -63,17 +51,38 @@ def login():
             print(f"current value of desired_value: {desired_value}")
         except Exception as e:
             print (f"Cannot print desired_value, error: {e}")
-        print(f"current value of verify: {verify}")
 
-        if (username == "admin") and (password == "admin") and (int(verify) == desired_value):
-            print("Credentials are correct")
-            error = None
-            return redirect("/")
+        if request.method == "POST":
+            print("This is happening only in POST")
+            desired_value = a + b
+            
+            print("Getting username, password and verify value...")
+            username = request.form.get('username')
+            session["username"] = username
+            password = request.form.get('password')
+            verify = request.form.get('verify')
+
+            print("Current values used for checking:")
+            print(f"current value of a: {a}")
+            print(f"current value of b: {b}")
+            print(f"current value of username: {username}")
+            try:
+                print(f"current value of desired_value: {desired_value}")
+            except Exception as e:
+                print (f"Cannot print desired_value, error: {e}")
+            print(f"current value of verify: {verify}")
+
+            if (username == "admin") and (password == "admin") and (int(verify) == desired_value):
+                print("Credentials are correct")
+                logged_in = True
+                session["logged_in"]=logged_in
+                error = None
+                return redirect("/"), logged_in
+            else:
+                error = "Invalid credentials"
+                return redirect("/login")
         else:
-            error = "Invalid credentials"
-            return redirect("/login")
-    else:
-        return render_template("login.html", a=a, b=b, username=username)
+            return render_template("login.html", a=a, b=b, username=username)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
